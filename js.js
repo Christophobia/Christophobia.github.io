432c8ef8d62c35d8846ea980a91fbc81a62a655b
Christophobia <56790624+Christophobia@users.noreply.github.com>
2024-07-24 13:34:42 +0800

    i hate it here
    i tried my best

---
+++ js.js
@@ -0,0 +1,37 @@
+const page1btn = document.querySelector("#page1btn");
+const page2btn = document.querySelector("#page2btn");
+const page3btn = document.querySelector("#page3btn");
+const hamBtn = document.querySelector("#hamicon");
+const hamMenu_ul = document.querySelector("nav ul");
+var pages = document.querySelectorAll(".page");
+
+console.log(pages);
+
+// hide all by default
+HideAll();
+
+// add event listeners
+page1btn.addEventListener("click", function () { Show(1); });
+page2btn.addEventListener("click", function () { Show(2); });
+page3btn.addEventListener("click", function () { Show(3); });
+
+hamBtn.addEventListener("click", ToggleMenu);
+
+// functions
+function HideAll() {
+  for (let p of pages) {
+    p.style.display = "none";
+  }
+}
+
+function Show(pgno) {
+  HideAll();
+  let p = document.querySelector("#page" + pgno);
+  p.style.display = "block";
+}
+
+function ToggleMenu() {
+  if (hamMenu_ul.style.display == "block")
+    hamMenu_ul.style.display = "none";
+  else hamMenu_ul.style.display = "block";
+}
